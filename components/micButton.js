a203e11fe5d173b8673a4d266e4f80d040370964
Marouane <MarouaneBenbetka@gmail.com>
2023-03-11 18:03:59 +0100

    voice integration

--- components/micButton.js
+++ components/micButton.js
@@ -1,33 +1,31 @@
 import React from 'react';
 import {StyleSheet, TouchableOpacity, Image} from 'react-native';
 
-const micButton = ({onPress, imageSource}) => {
+export default function MicButton({onPress, imageSource}) {
   return (
     <TouchableOpacity onPress={onPress} style={styles.circleButton}>
       <Image source={imageSource} style={styles.image} resizeMode="contain" />
     </TouchableOpacity>
   );
-};
+}
 
 const styles = StyleSheet.create({
   circleButton: {
     justifyContent: 'center',
     alignItems: 'center',
-    width: 54.12,
-    height: 54.12,
+    width: 54,
+    height: 54,
     borderRadius: 35,
     backgroundColor: '#FF0042',
     backgroundOpacity: '80',
     shadowColor: '#000',
     shadowOffset: {width: 0, height: 2},
     shadowOpacity: 0.25,
-    shadowRadius: 3.84,
+    shadowRadius: 4,
     elevation: 5,
   },
   image: {
-    width: 35.3,
-    height: 35.89,
+    width: 35,
+    height: 36,
   },
 });
-
-export default micButton;
